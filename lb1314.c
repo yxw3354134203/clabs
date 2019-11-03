229916bfb1798344550a77c96da0271bdfd97019
yxw3354134203 <https://github.com/yxw3354134203/clabs.git>
2019-11-03 06:04:01 -0800

    111

---
+++ lb1314.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int celsius,fahr;
+    int lower,upper,step;
+
+    lower=0;
+    upper=100;
+    step=5;
+
+    celsius=lower;
+    while(celsius<=upper){
+        fahr=celsius*9/5+32;
+        printf("%d\t%d\n",celsius,fahr);
+        celsius=celsius+step;
+    }
+} 
