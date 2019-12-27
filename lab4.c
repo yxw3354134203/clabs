53afa12c838523c4e82b0c5458c4a43db26859e9
yxw3354134203 <https://github.com/yxw3354134203/clabs.git>
2019-12-26 21:17:59 -0800

    111

--- lab4.c
+++ lab4.c
@@ -1,157 +1,156 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-#define MAXOP  100
-#define NUMBER  '0'
-#define MAXVAL 100
-#define BUFSIZE 100
-
+#define MAXOP 100
+#define NUMBER '0'
 int getop(char[]);
 void push(double);
-double pop(void);
-int getch(void);
-void ungetch(int);
-void n(void);
-
+double pop(void); 
+double top(void);
+extern int sp;
+int main()
+{ 
+    int type;
+    double op2, op0, copy; 
+    char s[MAXOP];
+    while ((type = getop(s)) != EOF)
+    {
+        switch (type)
+        {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0) 
+                push(pop() / op2);
+            else
+                printf("errer:zero divisor\n");
+            break;
+        case '%': /*remainder*/
+            op2 = pop();
+            if (op2 != 0)
+                push(((int)pop()) % ((int)op2));
+            else
+                printf("errer:zero divisor\n");
+            break;
+        case '~': /*print the top factot*/
+            printf("%.8g\n", top());
+            break;
+        case '$': /*exchange the topest two factors*/
+            op2 = pop();
+            op0 = pop(); 
+            push(op2);
+            push(op0);
+            break;
+        case '<': /*copy the top factor*/
+            copy = top();
+            push(copy);
+            break;
+        case '>': /*empty the val*/
+            sp = 0;
+            break; 
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error:unknow command %s\n", s);
+            break;
+        }
+    }
+    return 0;
+}
+#define MAXVAL 100
 int sp = 0;
 double val[MAXVAL];
-char buf[BUFSIZE];
-int bufp = 0;
-
-
-
-
-void n(void){
-    sp = 0;
-}
-
 void push(double f)
 {
     if (sp < MAXVAL)
-     {
-         val[sp++] = f;
-     }
-     else
-    {
-        printf("error:stack full ,can not push %g\n",f);
-    }
+        val[sp++] = f;
+    else
+        printf("error:stack full,can not push %g\n", f);
 }
 double pop(void)
 {
-    if(sp > 0)
-       {
-           return val[--sp];
-       }   
-    else{
+    if (sp > 0)
+        return val[--sp];
+    else
+    {
         printf("error:stack empty\n");
         return 0.0;
     }
 }
-
-
-
+double top(void)
+{
+    double result;
+    if (sp > 0)
+    {
+        result = val[--sp];
+        ++sp;
+        return result;
+    }
+    else
+    {
+        printf("error:stack empty\n");
+        return 0.0;
+    }
+}
+#include <ctype.h>
+int getch(void);
+void ungetch(int);
 int getop(char s[])
 {
-    int i,c;
+    int i, c;
     while ((s[0] = c = getch()) == ' ' || c == '\t')
-         ;
-         s[1] = '\0';
-         if(!isdigit(c) && c != '.')
-             return c;
-        i = 0;   
-        if(isdigit(c))
-          while (isdigit(s[++i] = c = getch()))
-              ;
-        if (c == '.')
-          while (isdigit(s[++i] = c = getch()))
-              ;
-            s[i]='\0';
+        ;
+    s[1] = '\0';
+    i = 0;
+    if (!isdigit(c) && c != '.' && c != '-') /*dealing with negative numbers*/
+        return c;
+    if (c == '-')
+    {
+        if (isdigit(c = getch()))
+            s[++i] = c;
+        else
+        {
             if (c != EOF)
-               ungetch(c);
-               return NUMBER;
+            {
+                ungetch(c);
+            }
+            return '-';
         }
-
-
-
+    }
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
 int getch(void)
 {
-    return (bufp > 0)? buf[--bufp] : getchar();
+    return (bufp > 0) ? buf[--bufp] : getchar();
 }
 void ungetch(int c)
 {
     if (bufp >= BUFSIZE)
-         printf("ungetch:too many characters\n");
+        printf("ungetch:too many characters\n");
     else
-         buf[bufp++] = c;
-}
-
-int main()
-{
-    int type;
-    double op2,op1;
-    char s[MAXVAL];
-
-    while ((type = getop(s))!= EOF)
-    {
-        switch(type)
-        {
-            case NUMBER:
-               push(atof(s));
-               break;
-            case '+':
-               push(pop() + pop());
-               break;
-            case'*':
-               push(pop() * pop());
-               break;
-            case'-':
-               op2 = pop();
-               push(pop() - op2);
-               break;
-            case'%':
-               op2 = pop();
-               if(op2!=0)
-               push((int)pop() % (int)op2);
-               else
-               {
-                   printf("error:zero divisor\n");
-               }
-               
-               break;
-            case'/':
-               op2 = pop();
-               if(op2 != 0.0)
-                   push(pop() / op2);
-               else
-                    printf("error:zero divisor\n");
-                break;
-            case'\n':
-                printf("\t%.8g\n",pop());
-                break;
-            default:
-                printf("error:known command %s\n",s);
-                break;   
-
-            case'q':
-                op1 = pop();
-                printf("\t%.8g\n",op1);
-                push(op1);
-                break;
-            case'w':
-                op1 = pop();
-                push(op1);
-                push(op1);
-                break;
-            case'e':
-                op1 = pop();
-                op2 = pop();
-                push(op1);
-                push(op2);
-                break;
-            case'r':
-                n();
-                break;
-        }
-    }return 0;
+        buf[bufp++] = c;
 }
